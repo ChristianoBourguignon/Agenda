c527328aca0419d3b7c191c963d799d9530a9f40
ChristianoBourguignon <christianombl@gmail.com>
2025-03-20 13:26:53 -0300

    Projeto Agenda

---
+++ src/Agenda.java
@@ -0,0 +1,94 @@
+import java.io.*;
+
+public class Agenda {
+    String nome;
+    String numero;
+
+    public Agenda(String nome, String numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+    public String getNumero(){
+        return this.numero;
+    }
+    public void setNome(String nome){
+        this.nome = numero;
+    }
+    public void setNumero(String numero){
+        this.numero = numero;
+    }
+    public static void escrita(Agenda agenda){
+        try {
+            FileWriter fw = new FileWriter("agenda.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(agenda.getNome()+";"+agenda.getNumero());
+            bw.newLine();
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        };
+    };
+    public static void lerTodos() {
+        try{
+            FileReader fr = new FileReader("agenda.txt");
+            BufferedReader br = new BufferedReader(fr);
+            while(br.ready()){
+                String aux = br.readLine();
+                String [] dados = aux.split(";");
+                System.out.println("Nome: "+ dados[0] + " - Telefone: " + dados[1]);
+            }
+            br.close();
+            fr.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void lerContato(String nome) {
+        try{
+            FileReader fr = new FileReader("agenda.txt");
+            BufferedReader br = new BufferedReader(fr);
+            boolean encontrado = false;
+            while(br.ready()) {
+                String aux = br.readLine();
+                String[] dados = aux.split(";");
+                if (dados[0].equals(nome)){
+                    if (!encontrado) {
+                        System.out.println("Encontrado os seguintes contatos com o nome: " + nome);
+                        encontrado = true;
+                    }
+                    System.out.println("Nome: " + dados[0] + " - Telefone: " + dados[1]);
+                }
+            }
+            if (!encontrado){
+                System.out.println("Contato " + nome +" não encontrado.");
+            }
+            br.close();
+            fr.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void alterar(Agenda agenda){
+        try {
+            FileWriter fw = new FileWriter("agenda.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(agenda.getNome()+";"+agenda.getNumero());
+            bw.newLine();
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        };
+    };
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Agenda ctChristiano = new Agenda("Christiano", "123");
+        Agenda ctChristiano1 = new Agenda("Christiano", "1234");
+        //Agenda.escrita(ctChristiano);
+        //Agenda.escrita(ctChristiano1);
+        Agenda.lerTodos();
+        Agenda.lerContato(ctChristiano.getNome());
+    }
+}
